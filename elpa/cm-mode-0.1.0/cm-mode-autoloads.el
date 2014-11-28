bd212e13e213f04eaa2c2ec101fc8e2b81dc2513
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2014-11-28 07:00:20 +0100

    Rust support

---
+++ elpa/cm-mode-0.1.0/cm-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; cm-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (cm-mode) "cm-mode" "cm-mode.el" (21623 41614 866515
+;;;;;;  943000))
+;;; Generated autoloads from cm-mode.el
+
+(autoload 'cm-mode "cm-mode" "\
+
+
+\(fn MODE)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("cm-mode-pkg.el") (21623 41614 913197
+;;;;;;  16000))
+
+;;;***
+
+(provide 'cm-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; cm-mode-autoloads.el ends here
